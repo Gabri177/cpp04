d3ddf4c81b1136ee62a6f7fe0b848518a3beccd9
GGabriel003 <108942046+GGabriel003@users.noreply.github.com>
2024-05-09 05:15:44 +0200

    finish ex01

---
+++ ex02/Animal.hpp
@@ -0,0 +1,21 @@
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+# include <iostream>
+
+class Animal {
+
+	protected:
+		std::string 		type;
+	
+	public:
+							Animal(void);
+							Animal(std::string type);
+							Animal(const Animal &obj);
+		virtual				~Animal(void);
+		Animal&				operator=(const Animal &obj);
+		std::string			getType(void) const;
+		virtual	void		makeSound(void) const;
+
+};
+
+#endif
